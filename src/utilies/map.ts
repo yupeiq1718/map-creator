c45cda26b420d3bbc608d1883becc21d6e89aa95
PeiYu <yupeiq1718@gmail.com>
2022-10-27 17:11:44 +0800

    feat: modify image handler

---
+++ src/utilies/map.ts
@@ -0,0 +1,221 @@
+interface Map {
+  src:string,
+  ratio:number,
+  zoom:number,
+  offsetX:number,
+  offsetY:number
+}
+
+interface Coordinate {
+  zoom:number,
+  offsetX:number,
+  offsetY:number
+}
+
+interface Position {
+  x: number,
+  y: number,
+}
+
+interface HotSpot {
+  position: Position,
+  floor: number
+}
+
+interface CameraInformation {
+  x: number,
+  y: number,
+  angle: number
+}
+
+interface Props {
+  dom: HTMLElement,
+  maps: Map[],
+  hotSpots: HotSpot[],
+  coordinates: Coordinate[]
+}
+
+const useMapConverter = ({ dom, maps, hotSpots, coordinates }:Props) => {
+  const mapImage = document.createElement('img')
+  dom.appendChild(mapImage)
+
+  const canvas = document.createElement('div')
+  dom.appendChild(canvas)
+
+  const cameraCanvas = document.createElement('div')
+
+  const hotSpotCanvas = document.createElement('div')
+
+  const positionImage = document.createElement('img')
+  const angleImage = document.createElement('img')
+
+  let coordinateBaseX:number
+  let coordinateBaseY:number
+  let coordinateRatio:number
+  let coordinateRangeX:number
+  let coordinateRangeY:number
+
+  const calculateCoordinate = (positions:Position[]) => {
+    const positionX = positions.map((hotSpot:Position) => hotSpot.x)
+    const positionY = positions.map((hotSpot:Position) => hotSpot.y)
+
+    const maxCoordinateX = Math.max.apply(null, positionX)
+    const minCoordinateX = Math.min.apply(null, positionX)
+    const maxCoordinateY = Math.max.apply(null, positionY)
+    const minCoordinateY = Math.min.apply(null, positionY)
+    coordinateBaseX = minCoordinateX
+    coordinateBaseY = minCoordinateY
+    coordinateRangeX = maxCoordinateX - minCoordinateX
+    coordinateRangeY = maxCoordinateY - minCoordinateY
+    coordinateRatio = coordinateRangeX / coordinateRangeY
+  }
+
+  const setMap = (map: Map) => {
+    const { src, offsetX, offsetY, ratio, zoom } = map
+    mapImage.src = new URL(src, import.meta.url).href
+    mapImage.setAttribute('style', `position:absolute;left:${100 * offsetX}%;top:${100 * offsetY}%;width:${100 * zoom}%;aspect-ratio:${ratio}`)
+  }
+
+  const setCanvas = (coordinate:Coordinate) => {
+    const { offsetX, offsetY, zoom } = coordinate
+    canvas.setAttribute('style', `position:absolute;left:${100 * offsetX}%;top:${100 * offsetY}%;width:${100 * zoom}%;aspect-ratio:${coordinateRatio}`)
+  }
+
+  const getNormalizedPosition = ({ x, y }:{
+    x: number,
+    y: number
+  }) => ({
+    x: 100 * (x - coordinateBaseX) / coordinateRangeX,
+    y: 100 * (y - coordinateBaseY) / coordinateRangeY
+  })
+
+  const drawHotSpot = ({ x, y, size }:{
+    x: number, y: number, size: number
+  }) => {
+    const hotSpotImage = document.createElement('img')
+    hotSpotImage.src = hotSpotData.src
+    hotSpotImage.setAttribute('style', `position:absolute;left:${x}%;top:${y}% ;width:${size}px;height:${size}px;transform:translate(-50%,-50%);z-index:10;`)
+    hotSpotCanvas.appendChild(hotSpotImage)
+  }
+
+  let normalizedPositions:Position[]
+
+  let isHotSpotActive = false
+
+  interface HotSpotData {
+    size: number,
+    src: string
+  }
+
+  const hotSpotData = {
+    size: 6,
+    src: ''
+  }
+
+  const createHotSpots = ({ size, src }: HotSpotData) => {
+    hotSpotCanvas.setAttribute('style', 'position:absolute;top:0;left:0;width:100%;height:100%;')
+    hotSpotCanvas.setAttribute('id', 'hotSpotCanvas')
+    canvas.appendChild(hotSpotCanvas)
+    hotSpotData.size = size
+    hotSpotData.src = src
+    normalizedPositions.forEach(normalizedPosition => {
+      drawHotSpot({ size: hotSpotData.size, ...normalizedPosition })
+    })
+    isHotSpotActive = true
+  }
+
+  const updateHotSpots = () => {
+    const hotSpotCanvasDOM = document.querySelector('#hotSpotCanvas')
+    const children = hotSpotCanvasDOM?.childNodes || []
+    for (let i = children.length - 1; i >= 0; i--) {
+      hotSpotCanvasDOM?.removeChild(children[i])
+    }
+    normalizedPositions.forEach(normalizedPosition => {
+      drawHotSpot({ size: hotSpotData.size, ...normalizedPosition })
+    })
+  }
+
+  const deleteHotSpots = () => {
+    const hotSpotCanvasDOM = document.querySelector('#hotSpotCanvas')
+    const children = hotSpotCanvasDOM?.childNodes || []
+    for (let i = children.length - 1; i >= 0; i--) {
+      hotSpotCanvasDOM?.removeChild(children[i])
+    }
+    isHotSpotActive = false
+  }
+
+  interface PositionImageData {
+    size: number,
+    src: string
+  }
+  let positionImageData: PositionImageData = {
+    size: 6,
+    src: ''
+  }
+
+  interface AngleImageData {
+    size: number,
+    src: string
+  }
+  let angleImageData: AngleImageData = {
+    size: 20,
+    src: ''
+  }
+
+  const createCameraPosition = ({ positionData, angleData }:{
+    positionData: PositionImageData,
+    angleData: AngleImageData
+  }) => {
+    cameraCanvas.setAttribute('style', 'position:absolute;top:0;left:0;width:100%;height:100%;')
+    cameraCanvas.setAttribute('id', 'cameraCanvas')
+    canvas.appendChild(cameraCanvas)
+
+    positionImageData = positionData
+    positionImage.src = new URL('../assets/images/position.svg', import.meta.url).href
+    cameraCanvas.appendChild(positionImage)
+
+    angleImageData = angleData
+    angleImage.src = new URL('../assets/images/angle.svg', import.meta.url).href
+    cameraCanvas.appendChild(angleImage)
+  }
+
+  const updateCameraPosition = (cameraInformation: CameraInformation) => {
+    const { x, y } = getNormalizedPosition(cameraInformation)
+    positionImage.setAttribute('style', `position:absolute;left:${x}%;top:${y}% ;width:${positionImageData.size}px;height:${positionImageData.size}px;transform:translate(-50%,-50%);z-index:20;`)
+    angleImage.setAttribute('style', `position:absolute;left:calc(${x}% + 4px);top:calc(${y}%);width:${angleImageData.size}px;height:${angleImageData.size}px;transform:translateY(-50%) rotate(${360 - cameraInformation.angle * 180 / Math.PI - 90}deg); transform-origin:-4px 50%;z-index:20;`)
+  }
+
+  const deleteCameraPosition = () => {
+    const cameraCanvasDOM = document.querySelector('#cameraCanvas')
+    const children = cameraCanvasDOM?.childNodes || []
+    for (let i = children.length - 1; i >= 0; i--) {
+      cameraCanvasDOM?.removeChild(children[i])
+    }
+  }
+
+  const setFloor = (floor: number) => {
+    setMap(maps[floor])
+    const positions = hotSpots.filter(hotSpot => hotSpot.floor === floor).map(({ position }) => ({
+      x: position.x,
+      y: position.y * -1
+    }))
+    calculateCoordinate(positions)
+    setCanvas(coordinates[floor])
+    normalizedPositions = positions.map(position => getNormalizedPosition(position))
+    isHotSpotActive && updateHotSpots()
+  }
+
+  return {
+    createHotSpots,
+    updateHotSpots,
+    deleteHotSpots,
+    createCameraPosition,
+    updateCameraPosition,
+    deleteCameraPosition,
+    setFloor
+  }
+}
+
+export {
+  useMapConverter
+}
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,110 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+import { useMapConverter } from '@/utilies/map'
+import floor1Image from '@/assets/images/floor1.svg'
+import floor2Image from '@/assets/images/floor2.svg'
+import hotSpotImage from '@/assets/images/hot_spot.svg'
+import positionImage from '@/assets/images/position.svg'
+import angleImage from '@/assets/images/angle.svg'
 
+interface Position {
+  x: number,
+  y: number
+}
+
+interface HotSpot {
+  position: Position,
+  floor: number
+}
+
+const hotSpots:HotSpot[] = [
+  {
+    position: {
+      x: 0, y: 0
+    },
+    floor: 0
+  },
+  {
+    position: {
+      x: 21, y: 14
+    },
+    floor: 0
+  },
+  {
+    position: {
+      x: 77, y: 36
+    },
+    floor: 0
+  },
+  {
+    position: {
+      x: 99, y: 99
+    },
+    floor: 0
+  }
+]
+
+const maps = [
+  {
+    src: floor1Image,
+    ratio: 1.5853,
+    zoom: 0.56,
+    offsetX: 0.32,
+    offsetY: 0.29
+  },
+  {
+    src: floor2Image,
+    ratio: 1.6222,
+    zoom: 1,
+    offsetX: 0,
+    offsetY: 0.1
+  }
+]
+
+const coordinates = [
+  {
+    zoom: 1,
+    offsetX: 0,
+    offsetY: 0.14
+  },
+  {
+    zoom: 0.9,
+    offsetX: 0.03,
+    offsetY: 0.16
+  }
+]
+
+const mapDOM = ref()
+
+const handleMap = () => {
+  const { setFloor, createHotSpots, createCameraPosition, updateCameraPosition } = useMapConverter({
+    dom: mapDOM.value,
+    maps,
+    hotSpots,
+    coordinates: coordinates
+  })
+  setFloor(0)
+  createHotSpots({ size: 6, src: hotSpotImage })
+  createCameraPosition({
+    positionData: {
+      size: 6,
+      src: positionImage
+    },
+    angleData: {
+      size: 20,
+      src: angleImage
+    }
+  })
+}
+onMounted(async () => {
+  handleMap()
+})
+
+</script>
 <template>
-  <img
-    alt="Vue logo"
-    src="@/assets/images/logo.png"
-  >
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="fixed top-12 left-20 z-50 border-2 border-black w-1/4 h-1/4 rounded-2xl bg-black">
+    <div
+      ref="mapDOM"
+      class="absolute w-full h-full"
+    />
+  </div>
 </template>
